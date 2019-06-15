4729de953bc8a8a48652893ec071cf40519930c9
Kevin De Rosa <kevinderosa@Kevins-MBP-2.domain>
2019-06-14 20:02:42 -0700

    Updated files

---
+++ elon.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {myFunction()};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+{/* <script src="website.js"></script> */}
